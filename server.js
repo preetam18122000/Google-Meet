4f945739f00a4fe35f2f887fff0b62f80deded6d
Preetam Singh Dabi <preetamdabi1812@gmail.com>
2024-07-28 17:54:36 +0530

    Feat/added css and html modifications

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3030;
 const server = require('http').Server(app);
+const socketIo = require('socket.io');
+const io = socketIo(server);
 const { v4: uuidv4 } = require('uuid');
-const io = require('socket.io')(server);
 const { ExpressPeerServer } = require('peer'); //you will have to run two servers - one for the port, other for peerjs
 const url = require('url');
 const peerServer = ExpressPeerServer(server, {
@@ -18,9 +19,11 @@ app.set("view engine", "ejs");   //to use ejs files or to use view engine
 app.use("/public", express.static(path.join(__dirname, "static")));
 app.use('/peerjs', peerServer); //any url starting with /peerjs will call this function
 
+//routes
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, "static", "index.html")); //this get call will render out initial html
 })
+
 app.get('/join', (req,res) => { //if a person is coming to /join, then we should redirect it to any specific meeting id
     res.redirect(
         url.format({
@@ -30,34 +33,35 @@ app.get('/join', (req,res) => { //if a person is coming to /join, then we should
     );
 });
 
-app.get('/joinold', (req,res) => { //to enter a meeting when you already have a meeting link
+app.get('/joinold', (req, res) => {
     res.redirect(
         url.format({
-            pathname: req.params.meeting_id,
+            pathname: `/join/${req.query.meeting_id}`,
             query: req.query
         })
     );
 });
 
-app.get("/join/:rooms", (req, res) => { //for joining a room
-    res.render("room", { roomid: req.params.rooms, Myname: req.query.name }); //render the ejs file - my video block with my name will be shown
+app.get("/join/:rooms", (req, res) => {
+    res.render("room", { roomid: req.params.rooms, Myname: req.query.name });
 });
 
 io.on("connection", (socket) => {
     socket.on("join-room", (roomid, id, myName) => {
         socket.join(roomid);
-        socket.to(roomid).broadcast.emit("user-connected", id, myName);
+        socket.to(roomid).emit("user-connected", id, myName);
 
         socket.on("tellName", (myName) => {
-            socket.to(roomid).broadcast.emit("AddName", myName);
+            socket.to(roomid).emit("AddName", myName);
         });
 
         socket.on("disconnect", () => {
-            socket.to(roomid).broadcast.emit("user-disconnected", id);
+            socket.to(roomid).emit("user-disconnected", id);
         });
     });
 });
 
-app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:3030');
-  });
+// Start the server
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
--- static/main.js
+++ static/main.js
@@ -1,57 +1,63 @@
 const socket = io("/");
-const main_chat_window = document.getElementById('main_chat_window');
+const main__chat__window = document.getElementById("main__chat_window");
 const videoGrids = document.getElementById("video-grids");
-const myVideo = document.getElementById("video");
+const myVideo = document.createElement("video");
 const chat = document.getElementById("chat");
-
 let OtherUsername = "";
+
 myVideo.muted = true;
 
 //for popup that comes up whenever you load the meeting, to copy the room id, or close it -> it is written in ejs file
 window.onload = () => {
-    $(document).ready(function () {
+    $(document).ready(function() {
         $("#getCodeModal").modal("show");
-    })
-}
+    });
+};
 
 var peer = new Peer(undefined, { //undefined as there is no server path here
     path: "/peerjs",
     host: "/",
-    port: "3030"
+    port: "3030",
 });
-
 
 let myVideoStream;
 const peers = {}; //for all the people who joins my meet link
 //format -> peers[userId] = callObject (provided by peer)
 
-var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia; //1,2 ->google and other v8engine, 3 -> mozilla
+var getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
 
-navigator.mediaDevices.getUserMedia({
-    video: true, // that we will need both video and audio
-    audio: true
-}).then((stream) => {
-    //this handles my screen
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream, myname); //it will create the video tag, run the video on it, and have the user's name on it -< for me
 
-    socket.on("user-connected", (id, username) =>{
-        connectToNewUser(id, stream, username); //to create a new video box of another person
-        socket.emit("tellName", myname); //to braodcast everyone that another person have joined the room
-    });
 
-    socket.on("user-disconnected", (id)=> {
-        if(peers[id]) peers[id].close();
+navigator.mediaDevices
+    .getUserMedia({
+        video: true, // that we will need both video and audio
+        audio: true,
     })
-});
+    .then((stream) => {
+            //this handles my screen
 
-peer.on("call", (call) => { //when we get a call from another person
-    getUserMedia({  video: true, audio: true },
-        function (stream) { //this stream will by my stream
-            call.answer(stream); //acknowledge the call and pass our stream to another person
-            const video = document.createElement("video"); //for me to see another persons video
-            call.on("stream", (remoteStream) => {
-                addVideoStream (video, remoteStream, OtherUsername); //third argument will be the name of the person who joined
+        myVideoStream = stream;
+        addVideoStream(myVideo, stream, myname); //it will create the video tag, run the video on it, and have the user's name on it -< for me
+
+        socket.on("user-connected", (id, username) => {
+            connectToNewUser(id, stream, username); //to create a new video box of another person
+            socket.emit("tellName", myname); //to braodcast everyone that another person have joined the room
+        });
+
+        socket.on("user-disconnected", (id) => {
+            if (peers[id]) peers[id].close();
+        });
+    });
+    peer.on("call", (call) => { //when we get a call from another person
+        getUserMedia({ video: true, audio: true },
+            function (stream) { //this stream will by my stream
+                call.answer(stream); // Answer the call with an A/V stream.
+                const video = document.createElement("video"); //for me to see another persons video
+                call.on("stream", function(remoteStream) {
+                addVideoStream(video, remoteStream, OtherUsername);
             });
         },
         function(err) {
@@ -64,9 +70,9 @@ peer.on("open", (id) => {
     socket.emit("join-room", roomId, id, myname);
 });
 
+
 socket.on("AddName", (username) => {
     OtherUsername = username;
-    console.log('OtherUsername', OtherUsername);
 });
 
 const RemoveUnusedDivs = () => {
@@ -81,81 +87,77 @@ const RemoveUnusedDivs = () => {
 
 const connectToNewUser = (userId, streams, myname) => {
     const call = peer.call(userId, streams);
-    const video = document.createElement("video"); //creating new video element
+    const video = document.createElement("video");
     call.on("stream", (userVideoStream) => {
+        //       console.log(userVideoStream);
         addVideoStream(video, userVideoStream, myname);
-    })
-
+    });
     call.on("close", () => {
         video.remove(); //remove video tag from UI
         RemoveUnusedDivs(); //remove unused divs where video tag is deleted (like above)
-    })
-
+    });
     peers[userId] = call;
-}
+};
 
 const cancel = () => {
     $("#getCodeModal").modal("hide");
-}
+};
 
 const copy = async() => {
-    const roomId = document.getElementById("roomId").innerText;
-    await navigator.clipboard.writeText("http://localhost:3030/join/" + roomId);
-}
-
+    const roomid = document.getElementById("roomid").innerText;
+    await navigator.clipboard.writeText(roomid);
+};
 const invitebox = () => {
     $("#getCodeModal").modal("show");
-}
+};
+
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        document.getElementById("mic").style.color = "red";
+    } else {
+        document.getElementById("mic").style.color = "white";
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+};
+
+const VideomuteUnmute = () => {
+    const enabled = myVideoStream.getVideoTracks()[0].enabled;
+    console.log(getUserMedia);
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        document.getElementById("video").style.color = "red";
+    } else {
+        document.getElementById("video").style.color = "white";
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+};
+
 
 const addVideoStream = (videoEl, stream, name) => {
     videoEl.srcObject = stream;
     videoEl.addEventListener("loadedmetadata", () => {
         //it means the video tag is ready to play the video
+
         videoEl.play();
-    })
+    });
     //to show the name
     const h1 = document.createElement("h1");
     const h1name = document.createTextNode(name);
     h1.appendChild(h1name);
-
     const videoGrid = document.createElement("div");
     videoGrid.classList.add("video-grid");
-
     videoGrid.appendChild(h1);
     videoGrids.appendChild(videoGrid);
-
     videoGrid.append(videoEl);
     RemoveUnusedDivs();
-
     //if multiple users have joined, we should divide the UI between them
     let totalUsers = document.getElementsByTagName("video").length;
-    if(totalUsers > 1){
-        for(let i = 0; i<totalUsers; i++){
-            document.getElementsByTagName("video")[i].style.width = 100 / totalUsers + "%"
+    if (totalUsers > 1) {
+        for (let index = 0; index < totalUsers; index++) {
+            document.getElementsByTagName("video")[index].style.width =
+                100 / totalUsers + "%";
         }
     }
-}
-
-const muteUnmute = () => {
-    const enabled = myVideoStream.getAudioTracks()[0].enabled;
-    if(enabled){
-        myVideoStream.getAudioTracks()[0].enabled = false;
-        document.getElementById("mic").style.color = "red";
-    } else {
-        myVideoStream.getAudioTracks()[0].enabled = true;
-        document.getElementById("mic").style.color = "white";
-    }
-
-}
-
-const videoMuteUnmute = () => {
-    const enabled = myVideoStream.getVideoTracks()[0].enabled;
-    if(enabled){
-        myVideoStream.getVideoTracks()[0].enabled = false;
-        document.getElementById("video").style.color = "red";
-    } else {
-        myVideoStream.getVideoTracks()[0].enabled = true;
-        document.getElementById("video").style.color = "white";
-    }
-
-}
+};
